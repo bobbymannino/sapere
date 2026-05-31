cdb1b12f7f675fb77bcbff4186ae74562ee359fa
Bob Mannino <manninobobby@icloud.com>
2026-05-31 20:34:38 +0100

    table sql

---
+++ backend/migrations/0004_workspaces.sql
@@ -0,0 +1,14 @@
+CREATE TABLE workspaces (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title text NOT NULL CHECK (title ~ '^[a-zA-Z0-9_- ]+$'),
+  description text CHECK (LENGTH(description) < 0),
+  author_id int NOT NULL REFERENCES users (id),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE UNIQUE INDEX ON workspaces (author_id, title);
+
+CREATE INDEX ON workspaces (author_id, created_at);
+
+CREATE INDEX ON workspaces (author_id, updated_at);
