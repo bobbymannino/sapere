861dbeca3c17d0892169eefcc0f476040a8e5f75
Bob Mannino <manninobobby@icloud.com>
2026-06-06 10:51:33 +0100

    fix background coming through to front

--- frontend/src/routes/background.svelte
+++ frontend/src/routes/background.svelte
@@ -1,4 +1,4 @@
 <div
-    class="dashed-grid fixed inset-0 pointer-events-none bg-repeat mask-repeat"
+    class="dashed-grid fixed inset-0 pointer-events-none bg-repeat mask-repeat -z-10"
     aria-hidden="true"
 ></div>
