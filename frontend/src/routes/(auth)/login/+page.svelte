b1d05a0303e503aac317c4ab9d7f3ed6b27f669f
Bob Mannino <manninobobby@icloud.com>
2026-06-06 15:17:03 +0100

    add login page

--- frontend/src/routes/(auth)/login/+page.svelte
+++ frontend/src/routes/(auth)/login/+page.svelte
@@ -1,29 +1,65 @@
 <script lang="ts">
     import { enhance } from "$app/forms";
+    import Button from "$lib/components/button.svelte";
+    import Input from "$lib/components/input.svelte";
+    import Logo from "$lib/components/logo.svelte";
+    import { UnauthorizedApiError } from "$lib/api/errors";
+    import Alert from "$lib/components/alert.svelte";
     import type { PageProps } from "./$types";
+    import FormErrors from "$lib/components/form-errors.svelte";
 
     let { form }: PageProps = $props();
+
+    let pending = $state(false);
 </script>
 
-<form method="post" use:enhance class="stack p-4 bg-mantle m-4 text-txt">
-    {JSON.stringify(form, null, 2)}
-    <label for="email">Email or username</label>
-    <input
-        type="text"
-        name="email"
-        id="email"
-        placeholder="johnsmith@email.com"
-        class="border-2 border-crust"
-    />
+<div class="min-h-svh flex-center p-6">
+    <form
+        method="post"
+        use:enhance={() => {
+            if (pending) return;
+            pending = true;
+            return async ({ update }) => {
+                await update();
+                pending = false;
+            };
+        }}
+        class="stack w-full max-w-md p-6 bg-mantle border border-crust rounded-lg shadow-lg shadow-primary/10"
+    >
+        <Logo />
 
-    <label for="password">Password</label>
-    <input
-        type="password"
-        name="password"
-        id="password"
-        placeholder="Password"
-        class="border-2 border-crust"
-    />
+        <h1>Login</h1>
 
-    <button type="submit">Login</button>
-</form>
+        <div class="stack-1">
+            <label for="email">Email or username</label>
+            <Input
+                id="email"
+                required
+                name="email"
+                type="text"
+                defaultValue={form?.email}
+                placeholder="johnsmith@email.com"
+            />
+            <FormErrors name="username" error={form?.error} />
+            <FormErrors name="email" error={form?.error} />
+        </div>
+
+        <div class="stack-1">
+            <label for="password">Password</label>
+            <Input
+                required
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+            />
+            <FormErrors name="password" error={form?.error} />
+        </div>
+
+        <Button type="submit" {pending} disabled={pending}>Login</Button>
+
+        {#if form?.error instanceof UnauthorizedApiError}
+            <Alert type="error">Invalid credentials</Alert>
+        {/if}
+    </form>
+</div>
