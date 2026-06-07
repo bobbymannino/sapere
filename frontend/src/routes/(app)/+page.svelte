dd2962ffecfa27f5da14b9a7931cd4a49f4372db
Bob Mannino <manninobobby@icloud.com>
2026-06-07 15:21:20 +0100

    require user on / page

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -8,10 +8,6 @@
 </script>
 
 <div class="bg-mantle text-txt stack m-6 p-4">
-  {#if data.user}
-    <h1>Welcome, {data.user.username}</h1>
-    <Button href={resolve("/logout")} variant="destructive" data-sveltekit-preload-data="off">Logout</Button>
-  {:else}
-    <Button href={resolve("/login")}>Login</Button>
-  {/if}
+  <h1>Welcome, {data.user.username}</h1>
+  <Button href={resolve("/logout")} variant="destructive" data-sveltekit-preload-data="off">Logout</Button>
 </div>
