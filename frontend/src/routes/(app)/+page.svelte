d1b003c21da309a66f61b2c280bdbd3ae978fbc6
Bob Mannino <manninobobby@icloud.com>
2026-06-07 18:44:27 +0100

    add workspaces list

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
   import { resolve } from "$app/paths";
-  import Button from "$lib/components/button.svelte";
 
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
 </script>
 
-<div class="bg-mantle text-txt stack m-6 p-4">
-  <h1>Welcome, {data.user.username}</h1>
-  <Button href={resolve("/logout")} variant="destructive" data-sveltekit-preload-data="off">Logout</Button>
-</div>
+<div class="p-6 stack">
+  <h1>Workspaces</h1>
 
-{#each { length: 10 }}
-  <div class="p-4">
-    <h1>Title</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores architecto nisi sunt, repellendus mollitia
-      amet modi ex, illo neque illum maiores voluptatum eum distinctio, consectetur eos omnis suscipit eligendi corrupti
-      odio vel odit. Quis vero ea numquam ratione saepe tempore, veritatis quisquam natus, minima nisi rem iure
-      repellat! Voluptate, dolore!
-    </p>
-  </div>
-{/each}
+  <ol class='stack'>
+    {#each data.workspaces.items as w (w.id)}
+        {const href = resolve("/workspaces/[slug]", { slug: `${w.slug}-${w.id}` })}
+      <li>
+        <a {href} class='rounded-md p-6 border border-crust bg-mantle block hover:scale-102 motion-safe:not-hover:transition-transform'>
+            <h3>
+                {w.title}
+            </h3>
+            {#if w.description}
+                <p>{w.description}</p>
+            {/if}
+            <p>
+                <small>Created {w.createdAt.toLocaleString()} • Updated {w.updatedAt.toLocaleString()}</small>
+            </p>
+        </a>
+      </li>
+    {/each}
+  </ol>
+</div>
