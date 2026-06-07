7617871d711e69b107629684f21dcc4bc6e5f666
Bob Mannino <manninobobby@icloud.com>
2026-06-07 17:33:32 +0100

    add workspaces create/list endpoints

--- frontend/src/lib/server/api/index.ts
+++ frontend/src/lib/server/api/index.ts
@@ -1,5 +1,14 @@
 import * as auth from "./auth";
+import * as workspaces from "./workspaces";
 
-export const api = { auth };
+export const api = { auth, workspaces };
 
 export type { User } from "./auth";
+
+export type {
+  CreateWorkspaceBody,
+  ListWorkspacesOptions,
+  ListWorkspacesResponse,
+  Workspace,
+  WorkspaceSortBy,
+} from "./workspaces";
