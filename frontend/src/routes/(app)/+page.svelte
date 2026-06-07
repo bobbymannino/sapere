d6ab28d15f49d1a6e64e8ddc02b8feece57535c8
Bob Mannino <manninobobby@icloud.com>
2026-06-07 17:07:11 +0100

    mobile header

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -11,3 +11,15 @@
   <h1>Welcome, {data.user.username}</h1>
   <Button href={resolve("/logout")} variant="destructive" data-sveltekit-preload-data="off">Logout</Button>
 </div>
+
+{#each { length: 10 }}
+  <div class="p-4">
+    <h1>Title</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores architecto nisi sunt, repellendus mollitia
+      amet modi ex, illo neque illum maiores voluptatum eum distinctio, consectetur eos omnis suscipit eligendi corrupti
+      odio vel odit. Quis vero ea numquam ratione saepe tempore, veritatis quisquam natus, minima nisi rem iure
+      repellat! Voluptate, dolore!
+    </p>
+  </div>
+{/each}
