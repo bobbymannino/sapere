e58d4541173540a4d8f927d04faf862c4db840af
Bob Mannino <manninobobby@icloud.com>
2026-06-18 08:51:19 +0100

    add APP_NAME var

--- src/env.ts
+++ src/env.ts
@@ -3,9 +3,17 @@ import * as v from "valibot";
 
 const DatabaseUrlSchema = v.pipe(v.string(), v.url(), v.startsWith("postgres://"));
 
+const AppNameSchema = v.pipe(v.string(), v.minLength(1));
+
 export const variables = defineEnvVars({
   DATABASE_URL: {
     schema: DatabaseUrlSchema,
     description: "Postgres database connection URL",
+  },
+  APP_NAME: {
+    schema: AppNameSchema,
+    description: "The name of the project",
+    public: true,
+    static: true,
   },
 });
