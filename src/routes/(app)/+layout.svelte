2254df509cab6b88a0d04344a536df22c740636e
Bob Mannino <manninobobby@icloud.com>
2026-06-18 20:16:52 +0100

    sidebar footer

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import SidebarProvider from "$lib/components/ui/sidebar/sidebar-provider.svelte";
     import SidebarTrigger from "$lib/components/ui/sidebar/sidebar-trigger.svelte";
+    import type { LayoutProps } from "./$types";
     import AppSidebar from "./app-sidebar.svelte";
 
-    let { children } = $props();
+    let { children, data }: LayoutProps = $props();
 </script>
 
 <SidebarProvider>
-    <AppSidebar />
+    <AppSidebar username={data.session.user.username} />
     <main>
         <SidebarTrigger />
         {@render children?.()}
--- src/routes/(app)/app-sidebar.svelte
+++ src/routes/(app)/app-sidebar.svelte
@@ -1,57 +1,50 @@
 <script lang="ts">
-    import ErrorIcon from "$lib/icons/error-icon.svelte";
-    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import DropdownMenu from "$lib/components/ui/dropdown-menu/dropdown-menu.svelte";
+    import DropdownMenuContent from "$lib/components/ui/dropdown-menu/dropdown-menu-content.svelte";
+    import DropdownMenuTrigger from "$lib/components/ui/dropdown-menu/dropdown-menu-trigger.svelte";
+    import DropdownMenuItem from "$lib/components/ui/dropdown-menu/dropdown-menu-item.svelte";
+    import ChevronUpIcon from "$lib/icons/chevron-up-icon.svelte";
+    import SidebarHeader from "$lib/components/ui/sidebar/sidebar-header.svelte";
+    import SidebarContent from "$lib/components/ui/sidebar/sidebar-content.svelte";
+    import Sidebar from "$lib/components/ui/sidebar/sidebar.svelte";
+    import SidebarFooter from "$lib/components/ui/sidebar/sidebar-footer.svelte";
+    import SidebarMenu from "$lib/components/ui/sidebar/sidebar-menu.svelte";
+    import SidebarMenuItem from "$lib/components/ui/sidebar/sidebar-menu-item.svelte";
+    import SidebarMenuButton from "$lib/components/ui/sidebar/sidebar-menu-button.svelte";
 
-    // Menu items.
-    const items = [
-        {
-            title: "Home",
-            url: "#",
-            icon: ErrorIcon,
-        },
-        {
-            title: "Inbox",
-            url: "#",
-            icon: ErrorIcon,
-        },
-        {
-            title: "Calendar",
-            url: "#",
-            icon: ErrorIcon,
-        },
-        {
-            title: "Search",
-            url: "#",
-            icon: ErrorIcon,
-        },
-        {
-            title: "Settings",
-            url: "#",
-            icon: ErrorIcon,
-        },
-    ];
+    type Props = { username: string };
+
+    let { username }: Props = $props();
 </script>
 
-<Sidebar.Root>
-    <Sidebar.Content>
-        <Sidebar.Group>
-            <Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
-            <Sidebar.GroupContent>
-                <Sidebar.Menu>
-                    {#each items as item (item.title)}
-                        <Sidebar.MenuItem>
-                            <Sidebar.MenuButton>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <item.icon />
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.MenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-    </Sidebar.Content>
-</Sidebar.Root>
+<Sidebar>
+    <SidebarHeader />
+    <SidebarContent />
+    <SidebarFooter>
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        {#snippet child({ props })}
+                            <SidebarMenuButton
+                                {...props}
+                                class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                            >
+                                {username}
+                                <ChevronUpIcon class="ms-auto" />
+                            </SidebarMenuButton>
+                        {/snippet}
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                        side="top"
+                        class="w-(--bits-dropdown-menu-anchor-width)"
+                    >
+                        <DropdownMenuItem>
+                            <span>Sign out</span>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </SidebarMenuItem>
+        </SidebarMenu>
+    </SidebarFooter>
+</Sidebar>
