4b0134842e37d4b8d942ffb2ce6fb394a8efb008
Bob Mannino <manninobobby@icloud.com>
2026-06-18 16:29:48 +0100

    add dummy sidebar

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import SidebarProvider from "$lib/components/ui/sidebar/sidebar-provider.svelte";
+    import SidebarTrigger from "$lib/components/ui/sidebar/sidebar-trigger.svelte";
+    import AppSidebar from "./app-sidebar.svelte";
+
+    let { children } = $props();
+</script>
+
+<SidebarProvider>
+    <AppSidebar />
+    <main>
+        <SidebarTrigger />
+        {@render children?.()}
+    </main>
+</SidebarProvider>
---
+++ src/routes/(app)/app-sidebar.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import ErrorIcon from "$lib/icons/error-icon.svelte";
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+
+    // Menu items.
+    const items = [
+        {
+            title: "Home",
+            url: "#",
+            icon: ErrorIcon,
+        },
+        {
+            title: "Inbox",
+            url: "#",
+            icon: ErrorIcon,
+        },
+        {
+            title: "Calendar",
+            url: "#",
+            icon: ErrorIcon,
+        },
+        {
+            title: "Search",
+            url: "#",
+            icon: ErrorIcon,
+        },
+        {
+            title: "Settings",
+            url: "#",
+            icon: ErrorIcon,
+        },
+    ];
+</script>
+
+<Sidebar.Root>
+    <Sidebar.Content>
+        <Sidebar.Group>
+            <Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+            <Sidebar.GroupContent>
+                <Sidebar.Menu>
+                    {#each items as item (item.title)}
+                        <Sidebar.MenuItem>
+                            <Sidebar.MenuButton>
+                                {#snippet child({ props })}
+                                    <a href={item.url} {...props}>
+                                        <item.icon />
+                                        <span>{item.title}</span>
+                                    </a>
+                                {/snippet}
+                            </Sidebar.MenuButton>
+                        </Sidebar.MenuItem>
+                    {/each}
+                </Sidebar.Menu>
+            </Sidebar.GroupContent>
+        </Sidebar.Group>
+    </Sidebar.Content>
+</Sidebar.Root>
