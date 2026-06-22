9a0a447d5afe634f093bd31e551d734a794308ca
Bob Mannino <manninobobby@icloud.com>
2026-06-22 12:14:05 +0100

    add breadcrumbs to slug page

--- src/routes/(app)/workspaces/[slug]/+page.svelte
+++ src/routes/(app)/workspaces/[slug]/+page.svelte
@@ -1,8 +1,35 @@
 <script lang="ts">
+    import BreadcrumbItem from "$lib/components/ui/breadcrumb/breadcrumb-item.svelte";
+    import BreadcrumbLink from "$lib/components/ui/breadcrumb/breadcrumb-link.svelte";
+    import BreadcrumbList from "$lib/components/ui/breadcrumb/breadcrumb-list.svelte";
+    import BreadcrumbPage from "$lib/components/ui/breadcrumb/breadcrumb-page.svelte";
+    import BreadcrumbSeparator from "$lib/components/ui/breadcrumb/breadcrumb-separator.svelte";
+    import Breadcrumb from "$lib/components/ui/breadcrumb/breadcrumb.svelte";
+    import SidebarTrigger from "$lib/components/ui/sidebar/sidebar-trigger.svelte";
     import type { PageProps } from "./$types";
 
     let { data, form }: PageProps = $props();
 </script>
 
-<h1>{data.workspace.title}</h1>
-<p>{data.workspace.updatedAt}</p>
+<header class="p-5">
+    <Breadcrumb>
+        <BreadcrumbList>
+            <BreadcrumbItem>
+                <SidebarTrigger />
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+                <BreadcrumbLink href="/workspaces">Workspaces</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+                <BreadcrumbPage>{data.workspace.title}</BreadcrumbPage>
+            </BreadcrumbItem>
+        </BreadcrumbList>
+    </Breadcrumb>
+</header>
+
+<div class="p-5">
+    <h1>{data.workspace.title}</h1>
+    <p>{data.workspace.updatedAt}</p>
+</div>
