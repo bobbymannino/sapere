693576c6018ca6184b03d1258508a55d68016b2d
Bob Mannino <manninobobby@icloud.com>
2026-06-22 12:38:07 +0100

    fix auth path in hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,16 @@
 import { building } from "$app/env";
 import { auth } from "$lib/auth";
 import type { Handle } from "@sveltejs/kit";
-import { svelteKitHandler } from "better-auth/svelte-kit";
+import { isAuthPath, svelteKitHandler } from "better-auth/svelte-kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  event.locals.session = await auth.api.getSession({
-    headers: event.request.headers,
-  });
+  event.locals.session = null;
+
+  if (!building && !isAuthPath(event.url.toString(), auth.options)) {
+    event.locals.session = await auth.api.getSession({
+      headers: event.request.headers,
+    });
+  }
 
   return svelteKitHandler({ event, resolve, auth, building });
 };
