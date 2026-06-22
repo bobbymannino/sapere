d7cf21eddc2c3dd4fa2881c0cedd374caadb3a2f
Bob Mannino <manninobobby@icloud.com>
2026-06-22 11:38:52 +0100

    very basic workspaces list

---
+++ src/lib/server/db/workspaces.ts
@@ -0,0 +1,43 @@
+import { db as mdb } from "$db";
+import { asc, desc, eq } from "drizzle-orm";
+import { BunSQLDatabase } from "drizzle-orm/bun-sql/postgres";
+
+import * as s from "./schema";
+
+type CommonArgs = {
+  db?: BunSQLDatabase;
+};
+
+type LiteralUnion<T extends string> = T | (string & {});
+
+type ListWorkspacesArgs = CommonArgs & {
+  /** @default -updatedAt */
+  orderBy?: null | LiteralUnion<"updatedAt" | "-updatedAt" | "createdAt" | "-createdAt" | "title" | "-title">;
+  ownerId: typeof s.workspaces.$inferSelect.ownerId;
+};
+
+export async function listWorkspaces(args: ListWorkspacesArgs) {
+  const db = args.db ?? mdb;
+  let orderBy = desc(s.workspaces.updatedAt);
+  if (args.orderBy) {
+    if (args.orderBy === "title") orderBy = asc(s.workspaces.title);
+    else if (args.orderBy === "-title") orderBy = desc(s.workspaces.title);
+    else if (args.orderBy === "createdAt") orderBy = asc(s.workspaces.createdAt);
+    else if (args.orderBy === "-createdAt") orderBy = desc(s.workspaces.createdAt);
+    else if (args.orderBy === "updatedAt") orderBy = asc(s.workspaces.updatedAt);
+  }
+
+  const spaces = await db
+    .select({
+      id: s.workspaces.id,
+      title: s.workspaces.title,
+      slug: s.workspaces.slug,
+      updatedAt: s.workspaces.updatedAt,
+      createdAt: s.workspaces.createdAt,
+    })
+    .from(s.workspaces)
+    .where(eq(s.workspaces.ownerId, args.ownerId))
+    .orderBy(orderBy);
+
+  return spaces;
+}
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,6 +5,9 @@
     import BreadcrumbSeparator from "$lib/components/ui/breadcrumb/breadcrumb-separator.svelte";
     import Breadcrumb from "$lib/components/ui/breadcrumb/breadcrumb.svelte";
     import SidebarTrigger from "$lib/components/ui/sidebar/sidebar-trigger.svelte";
+    import type { PageProps } from "./$types";
+
+    let { data }: PageProps = $props();
 </script>
 
 <header class="p-5">
@@ -20,3 +23,14 @@
         </BreadcrumbList>
     </Breadcrumb>
 </header>
+
+<ul>
+    {#each data.workspaces as w (w.id)}
+        <li>
+            <a href="/workspaces/{w.slug}-{w.id}">
+                <h2>{w.title}</h2>
+                <p>{w.updatedAt}</p>
+            </a>
+        </li>
+    {/each}
+</ul>
