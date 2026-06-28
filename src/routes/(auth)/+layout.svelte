7b0f86997748bc08d12d34736cec157454b96149
Bob Mannino <manninobobby@icloud.com>
2026-06-28 18:16:32 +0100

    ui: add new favicon

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import Card from "$lib/components/ui/card/card.svelte";
+    import Logo from "$lib/components/logo.svelte";
 
     let { children } = $props();
 </script>
 
-<main class="flex-center p-5">
+<main class="flex-center p-5 flex-col gap-5">
+    <Logo class="text-primary w-20" />
     <Card class="max-w-lg w-full">
         {@render children()}
     </Card>
