7edbdf54a784bc39943e3cb43a4cb8bc187d2cf3
Bob Mannino <manninobobby@icloud.com>
2026-06-28 16:43:11 +0100

    refactor: move out pagination into own file

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,29 +1,15 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
     import { resolve } from "$app/paths";
-    import { navigating, page } from "$app/state";
     import { Button } from "$lib/components/ui/button";
     import Empty from "$lib/components/empty.svelte";
-    import Pagination from "$lib/components/ui/pagination/pagination.svelte";
     import type { PageProps } from "./$types";
     import WorkspaceCard from "./workspace-card.svelte";
-    import PaginationLink from "$lib/components/ui/pagination/pagination-link.svelte";
-    import PaginationItem from "$lib/components/ui/pagination/pagination-item.svelte";
-    import { PaginationContent } from "$lib/components/ui/pagination";
-    import PaginationNext from "$lib/components/ui/pagination/pagination-next.svelte";
-    import PaginationPrevious from "$lib/components/ui/pagination/pagination-previous.svelte";
-    import PaginationEllipsis from "$lib/components/ui/pagination/pagination-ellipsis.svelte";
     import { WorkspaceIcon } from "$lib/icons";
     import Filters from "./filters.svelte";
+    import Pagination from "./pagination.svelte";
 
     let { data }: PageProps = $props();
     let workspaces = $derived(data.workspaces);
-
-    function onPageChange(newPage: number) {
-        const sp = new URLSearchParams(page.url.searchParams);
-        sp.set("page", newPage.toString());
-        goto(`${page.url.pathname}?${sp.toString()}`);
-    }
 </script>
 
 <div class="flex flex-col">
@@ -44,40 +30,6 @@
             </ul>
         </section>
 
-        <Pagination
-            count={workspaces.total}
-            perPage={workspaces.perPage}
-            page={workspaces.page}
-            {onPageChange}
-            class="mbs-auto p-5 pbs-0"
-        >
-            {#snippet children({ pages, currentPage })}
-                <PaginationContent>
-                    <PaginationItem>
-                        <PaginationPrevious disabled={Boolean(navigating.to)} />
-                    </PaginationItem>
-                    {#each pages as page (page.key)}
-                        {#if page.type === "ellipsis"}
-                            <PaginationItem>
-                                <PaginationEllipsis />
-                            </PaginationItem>
-                        {:else}
-                            <PaginationItem>
-                                <PaginationLink
-                                    {page}
-                                    isActive={currentPage === page.value}
-                                    disabled={Boolean(navigating.to)}
-                                >
-                                    {page.value}
-                                </PaginationLink>
-                            </PaginationItem>
-                        {/if}
-                    {/each}
-                    <PaginationItem>
-                        <PaginationNext disabled={Boolean(navigating.to)} />
-                    </PaginationItem>
-                </PaginationContent>
-            {/snippet}
-        </Pagination>
+        <Pagination count={workspaces.total} perPage={workspaces.perPage} page={workspaces.page} />
     {/if}
 </div>
---
+++ src/routes/(app)/pagination.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { navigating, page as svPage } from "$app/state";
+    import * as Pagination from "$lib/components/ui/pagination";
+
+    function onPageChange(newPage: number) {
+        const sp = new URLSearchParams(svPage.url.searchParams);
+        sp.set("page", newPage.toString());
+        goto(`${svPage.url.pathname}?${sp.toString()}`);
+    }
+
+    type Props = {
+        count: number;
+        perPage: number;
+        page: number;
+    };
+
+    let { count, page, perPage }: Props = $props();
+</script>
+
+<Pagination.Root {count} {perPage} {page} {onPageChange} class="mbs-auto p-5 pbs-0">
+    {#snippet children({ pages, currentPage })}
+        <Pagination.Content>
+            <Pagination.Item>
+                <Pagination.Previous disabled={Boolean(navigating.to)} />
+            </Pagination.Item>
+            {#each pages as page (page.key)}
+                {#if page.type === "ellipsis"}
+                    <Pagination.Item>
+                        <Pagination.Ellipsis />
+                    </Pagination.Item>
+                {:else}
+                    <Pagination.Item>
+                        <Pagination.Link {page} isActive={currentPage === page.value} disabled={Boolean(navigating.to)}>
+                            {page.value}
+                        </Pagination.Link>
+                    </Pagination.Item>
+                {/if}
+            {/each}
+            <Pagination.Item>
+                <Pagination.Next disabled={Boolean(navigating.to)} />
+            </Pagination.Item>
+        </Pagination.Content>
+    {/snippet}
+</Pagination.Root>
