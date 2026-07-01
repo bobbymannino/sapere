6a6133c4501ca20c2727cd66b741765a9767ee43
Bob Mannino <manninobobby@icloud.com>
2026-07-01 13:55:50 +0100

    feat: add global error page

--- src/lib/components/empty.svelte
+++ src/lib/components/empty.svelte
@@ -2,20 +2,34 @@
     import * as Empty from "$lib/components/ui/empty";
     import type { IconComponent } from "$lib/icons";
     import type { Snippet } from "svelte";
+    import { tv } from "tailwind-variants";
+
+    const empty = tv({
+        variants: {
+            color: {
+                default: "",
+                destructive: "bg-destructive/15 text-destructive",
+            },
+        },
+        defaultVariants: {
+            color: "default",
+        },
+    });
 
     type Props = {
         title: string;
         description: string;
         icon: IconComponent;
         children: Snippet;
+        color?: keyof typeof empty.variants.color;
     };
 
-    let { title, description, icon: Icon, children }: Props = $props();
+    let { title, description, icon: Icon, children, color }: Props = $props();
 </script>
 
 <Empty.Root>
     <Empty.Header>
-        <Empty.Media variant="icon">
+        <Empty.Media variant="icon" class={empty({ color })}>
             <Icon />
         </Empty.Media>
         <Empty.Title>{title}</Empty.Title>
