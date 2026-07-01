ec126378d6021d43b961bbe528218a3fea44b543
Bob Mannino <manninobobby@icloud.com>
2026-07-01 13:16:25 +0100

    fix: center auth pages

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -5,7 +5,7 @@
     let { children } = $props();
 </script>
 
-<main class="flex-center p-5 flex-col gap-5">
+<main class="flex-center flex-col min-h-svh p-5 gap-5">
     <Logo class="text-primary w-20" />
     <Card class="max-w-lg w-full">
         {@render children()}
