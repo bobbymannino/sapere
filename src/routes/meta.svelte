a5ece336c856ad2df1355ad7cd14019ea6d29455
Bob Mannino <manninobobby@icloud.com>
2026-07-05 11:06:18 +0100

    build: add favicon.ico

--- src/routes/meta.svelte
+++ src/routes/meta.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import { APP_NAME } from "$app/env/public";
-    import favicon from "$lib/assets/favicon.svg";
+    import faviconSvg from "$lib/assets/favicon.svg";
+    import faviconIco from "$lib/assets/favicon.ico";
 </script>
 
 <svelte:head>
     <title>{APP_NAME}</title>
-    <link rel="icon" href={favicon} />
+    <link rel="icon" href={faviconIco} sizes="144x144" type="image/x-icon" />
+    <link rel="icon" href={faviconSvg} type="image/svg+xml" />
     <meta name="application-name" content={APP_NAME} />
     <meta name="apple-mobile-web-app-title" content={APP_NAME} />
     <meta property="og:site_name" content={APP_NAME} />
