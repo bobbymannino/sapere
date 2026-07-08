7bcf004dfaa645aa127c80b13c4340b5e67e68fe
Bob Mannino <manninobobby@icloud.com>
2026-07-08 10:19:43 +0100

    ui: add github link to footer

--- src/routes/site-footer.svelte
+++ src/routes/site-footer.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
     import { APP_NAME } from "$app/env/public";
     import ThemeToggle from "$lib/components/theme-toggle.svelte";
+    import * as Button from "$lib/components/ui/button";
+    import { GitHubIcon } from "$lib/icons";
 
     const year = new Date().getFullYear();
 </script>
 
 <footer class="p-5 border-t border-border flex items-center justify-between">
+    <Button.Root
+        variant="ghost"
+        size="icon-sm"
+        href="https://github.com/bobbymannino/sapere"
+        title="Open GitHub repository"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        <GitHubIcon />
+    </Button.Root>
+
+    <ThemeToggle />
+
     <small class="text-muted-foreground">
         &copy; {year}
         {APP_NAME}
     </small>
-    <ThemeToggle />
 </footer>
