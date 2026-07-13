6cb3895179a3660d958185108e131ed66fa7a3e0
Bob Mannino <manninobobby@icloud.com>
2026-07-13 09:46:36 +0100

    chore: add table from shadcn

--- src/routes/(app)/account/+page.server.ts
+++ src/routes/(app)/account/+page.server.ts
@@ -1,5 +1,8 @@
+import { requireUser } from "$lib/server/auth-utils";
+
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = () => {
+  requireUser();
   return { breadcrumbs: [{ label: "Account" }] };
 };
