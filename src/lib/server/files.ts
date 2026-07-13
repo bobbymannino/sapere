ecf8f278f19be4c64a6d662ca70bb4926a6f3ff1
Bob Mannino <manninobobby@icloud.com>
2026-07-13 16:10:44 +0100

    fix: use minio for files in beta

--- src/lib/server/files.ts
+++ src/lib/server/files.ts
@@ -1,18 +1,18 @@
-import { dev } from "$app/env";
 import { MINIO_ACCESS_KEY_ID, MINIO_ENDPOINT_URL, MINIO_SECRET_ACCESS_KEY } from "$app/env/private";
 import { Files } from "files-sdk";
 import { fs } from "files-sdk/fs";
 import { minio } from "files-sdk/minio";
 
 export const files = new Files({
-  adapter: dev
-    ? fs({ root: "./.uploads" })
-    : minio({
-        bucket: "uploads",
-        endpoint: MINIO_ENDPOINT_URL ?? "",
-        accessKeyId: MINIO_ACCESS_KEY_ID,
-        secretAccessKey: MINIO_SECRET_ACCESS_KEY,
-      }),
+  adapter:
+    process.env.NODE_ENV === "development"
+      ? fs({ root: "./.uploads" })
+      : minio({
+          bucket: "uploads",
+          endpoint: MINIO_ENDPOINT_URL ?? "",
+          accessKeyId: MINIO_ACCESS_KEY_ID,
+          secretAccessKey: MINIO_SECRET_ACCESS_KEY,
+        }),
 });
 
 /**
