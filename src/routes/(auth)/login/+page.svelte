e7eb941fff27c5468a9f1c08d705fd50d3be60dc
Bob Mannino <manninobobby@icloud.com>
2026-07-13 08:28:42 +0100

    ui: add last login method to login form

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   import { resolve } from "$app/paths";
   import { page } from "$app/state";
+  import { authClient } from "$lib/auth-client";
   import Meta from "$lib/components/meta.svelte";
+  import { Badge } from "$lib/components/ui/badge";
   import CardContent from "$lib/components/ui/card/card-content.svelte";
   import CardFooter from "$lib/components/ui/card/card-footer.svelte";
   import CardHeader from "$lib/components/ui/card/card-header.svelte";
   import CardTitle from "$lib/components/ui/card/card-title.svelte";
+  import { slide } from "svelte/transition";
 
   import LoginForm from "./login-form.svelte";
+
+  const lastLoginMethod = $derived(authClient.getLastUsedLoginMethod());
 </script>
 
 <Meta title="Login" description="Log in to your account." robots="noindex,nofollow" />
@@ -29,4 +34,10 @@
       Signup
     </a>
   </p>
+
+  {#if lastLoginMethod}
+    <div in:slide>
+      <Badge variant="secondary">Last Login Method: {lastLoginMethod}</Badge>
+    </div>
+  {/if}
 </CardFooter>
