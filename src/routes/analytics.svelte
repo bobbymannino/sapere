6278672b51deecca4403a27b5b9f46075ad1ab52
Bob Mannino <manninobobby@icloud.com>
2026-07-22 15:27:05 +0100

    fix: only run analytics in production

--- src/routes/analytics.svelte
+++ src/routes/analytics.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  import { dev } from "$app/env";
   import { UMAMI_WEBSITE_ID } from "$app/env/public";
 </script>
 
 <svelte:head>
-  {#if !dev && UMAMI_WEBSITE_ID}
+  {#if UMAMI_WEBSITE_ID && process.env.NODE_ENV === "production"}
     <script
       defer
       src="https://analytics.bobman.dev/script.js"
